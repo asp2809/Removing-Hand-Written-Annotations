f01e0bfa38427a72223034394feec3aa027f2be7
Anshu S Panda <anshuspanda@gmail.com>
2018-10-05 01:49:41 +0530

    Modifying seedfind.py

--- seedfind.py
+++ seedfind.py
@@ -1,20 +1,26 @@
 import cv2
 import numpy
 import sys
+from PIL import Image
 from matplotlib import pyplot as plt
 from scipy.misc import toimage
+from copy import deepcopy
 
 sys.setrecursionlimit(1500000)
 
-src = cv2.imread('binimage.png', 0)
+src = cv2.imread('boundaries.png', 0)
 arr=numpy.ndarray.tolist(src)
+arrnew=[[255 for x in range(len(arr[0]))] for y in range(len(arr))]
+
 
 seed=[0]*8
 seedcount=0
 seedcoord=[]
 
 for i in range(len(arr)):
-    for j in range(len(arr[i])):
+    j=0
+    while(j<len(arr[i])):
+        # print(i, j)
         if arr[i][j]==0 and (j+7)<len(arr[i]):
             flag=1
             for k in range(len(seed)):
@@ -23,11 +29,10 @@ for i in range(len(arr)):
             if flag==1:
                 seedcount+=1
                 seedcoord.append([i, j])
-            j+=7
+                j+=7
+        j+=1
 
-print(seedcount)
-for y in seedcoord:
-    print(str(y[0]) + "\t" + str(y[1]))
+print(seedcoord)
 countleft=0
 countright=0
 
@@ -36,22 +41,25 @@ leftseedext=[]
 def countheightleft(i, j):
     global countleft
     if arr[i][j-1]==0 and j>0:
+        arrnew[i][j-1]=0
         j-=1
         countheightleft(i, j)
     elif arr[i-1][j-1]==0 and i>0 and j>0:
         if countleft<3:
+            arrnew[i-1][j-1]=0
             countleft+=1
         else:
-            leftseedext.append([i, j])
+            # leftseedext.append([i, j])
             return
         i-=1
         j-=1
         countheightleft(i, j)
     elif arr[i+1][j-1]==0 and i<(len(arr)-1) and j>0:
         if countleft<3:
+            arrnew[i+1][j-1]=0
             countleft+=1
         else:
-            leftseedext.append([i, j])
+            # leftseedext.append([i, j])
             return
         i+=1
         j-=1
@@ -63,22 +71,25 @@ rightseedext=[]
 def countheightright(i, j):
     global countright
     if arr[i][j+1]==0 and j<(len(arr[i])-1):
+        arrnew[i][j-1]=0
         j-=1
         countheightright(i, j)
     elif arr[i+1][j+1]==0 and i<(len(arr)-1) and j<(len(arr[i])-1):
         if countright<3:
+            arrnew[i-1][j-1]=0
             countright+=1
         else:
-            rightseedext.append([i, j])
+            # rightseedext.append([i, j])
             return 
         i+=1
         j+=1
         countheightright(i, j)
     elif arr[i-1][j+1]==0 and i<(len(arr)-1) and j>0:
         if countright<3:
+            arrnew[i+1][j-1]=0
             countright+=1
         else:
-            rightseedext.append([i, j])
+            # rightseedext.append([i, j])
             return 
         i-=1
         j+=1
@@ -89,9 +100,14 @@ validext=[]
 for x in seedcoord:
     countleft=0
     countright=0
+    for i in range(8):
+        arrnew[x[0]][x[1]+ i]=0
     countheightleft(x[0], x[1])
-    countheightright(x[0]+7, x[1]+7)
+    countheightright(x[0], x[1]+7)
 
-print("Seed Coordinate\t\tLeft Extension\t\tRight Extension")
-for x in range(len(seedcoord)):
-    print(str(seedcoord[x]) + "\t\t" + str(leftseedext[x]) + "\t\t" + str(rightseedext[x]))
+arr2=numpy.array(arrnew)
+print(arr2)
+plt.imshow(arr2)
+plt.show()
+img = toimage(arr2)
+img.save("annotations2.png")
